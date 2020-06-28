8a23f7b6a1d29b1377457be7cef47ca06b5d65f5
Jacob Henson <gammernut@aol.com>
2020-06-28 14:16:28 -0500

    Initial commit

---
+++ program.py
@@ -0,0 +1,10 @@
+
+print('----------------------------')
+print('         HELLO APP')
+print('----------------------------')
+print()
+
+user_text = input('what is your name?')
+greeting = 'nice to meet you ' + user_text
+
+print(greeting)
